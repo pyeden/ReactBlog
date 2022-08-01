734f67423325f78600d1d3307d7161edaff9ba37
Future Deng（豆姜） <future.deng@basebit.ai>
2022-08-01 19:16:51 +0800

    feat: 增加布局

---
+++ src/pages/ArticleList/components/EditableLinkGroup/index.jsx
@@ -0,0 +1,33 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { createElement } from 'react';
+import styles from './index.less';
+
+const EditableLinkGroup = (props) => {
+  const { links, linkElement, onAdd } = props;
+  return (
+    <div className={styles.linkGroup}>
+      {links.map((link) =>
+        createElement(
+          linkElement,
+          {
+            key: `linkGroup-item-${link.id || link.title}`,
+            to: link.href,
+            href: link.href,
+          },
+          link.title,
+        ),
+      )}
+      <Button size="small" type="primary" ghost onClick={onAdd}>
+        <PlusOutlined /> 添加
+      </Button>
+    </div>
+  );
+};
+
+EditableLinkGroup.defaultProps = {
+  links: [],
+  onAdd: () => {},
+  linkElement: 'a',
+};
+export default EditableLinkGroup;
--- src/pages/ArticleList/index.jsx
+++ src/pages/ArticleList/index.jsx
@@ -1,10 +1,38 @@
 import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
-import { Avatar, Image, List, message, Space } from 'antd';
+import { Avatar, Card, Col, Image, Link, List, message, Row, Space } from 'antd';
 import VirtualList from 'rc-virtual-list';
 import React, { useEffect, useRef, useState } from 'react';
 import { useModel } from 'umi';
 
+import EditableLinkGroup from './components/EditableLinkGroup';
+
 const ContainerHeight = document.body.scrollHeight - 260;
+const links = [
+  {
+    title: '操作一',
+    href: '',
+  },
+  {
+    title: '操作二',
+    href: '',
+  },
+  {
+    title: '操作三',
+    href: '',
+  },
+  {
+    title: '操作四',
+    href: '',
+  },
+  {
+    title: '操作五',
+    href: '',
+  },
+  {
+    title: '操作六',
+    href: '',
+  },
+];
 
 const App = () => {
   const [isData, setIsData] = useState(true);
@@ -54,48 +82,107 @@ const App = () => {
   };
 
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      dataSource={articleList}
-      footer={false}
-      loading={loading}
-      ref={listHeight}
-    >
-      <VirtualList
-        className={'rc-virtual-list-scrollbar_1 rc-virtual-list-scrollbar-show_1'}
-        data={articleList}
-        height={window.innerHeight - 230}
-        itemHeight={5}
-        itemKey="email"
-        onScroll={onScroll}
-      >
-        {(item) => (
-          <List.Item
-            key={item.title}
-            actions={[
-              <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-              <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-              <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-            ]}
-            extra={
-              <Image
-                width={272}
-                alt="logo"
-                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            }
+    <Row gutter={24}>
+      <Col xl={6} lg={24} md={24} sm={24} xs={24}>
+        <Card
+          style={{
+            marginBottom: 10,
+          }}
+          title="导航栏"
+          bordered={false}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
+        </Card>
+        <Card
+          style={{
+            marginBottom: 10,
+          }}
+          title="标签云"
+          bordered={false}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
+        </Card>
+        <Card
+          style={{
+            marginBottom: 10,
+          }}
+          title="近期文章"
+          bordered={false}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
+        </Card>
+        <Card
+          style={{
+            marginBottom: 10,
+          }}
+          title="浏览排行"
+          bordered={false}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
+        </Card>
+      </Col>
+      <Col xl={18} lg={24} md={24} sm={24} xs={24}>
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={articleList}
+          footer={false}
+          loading={loading}
+          ref={listHeight}
+          rowKey={'id'}
+        >
+          <VirtualList
+            data={articleList}
+            // height={window.innerHeight - 230}
+            height={2000}
+            itemHeight={5}
+            itemKey="id"
+            onScroll={onScroll}
           >
-            <List.Item.Meta
-              avatar={<Avatar src={item.avatar} />}
-              title={<a href={item.href}>{item.title}</a>}
-              description={item.description}
-            />
-            {item.content}
-          </List.Item>
-        )}
-      </VirtualList>
-    </List>
+            {(item) => (
+              <List.Item
+                key={item.id}
+                actions={[
+                  <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                  <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                  <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                ]}
+                extra={
+                  <Image
+                    width={272}
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                }
+              >
+                <List.Item.Meta
+                  avatar={
+                    <Avatar
+                      src={'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'}
+                    />
+                  }
+                  title={<a href={item.href}>{item.title}</a>}
+                  description={item.title}
+                />
+                {item.body}
+              </List.Item>
+            )}
+          </VirtualList>
+        </List>
+      </Col>
+    </Row>
   );
 };
 
